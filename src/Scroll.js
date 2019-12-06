2287e83b1b716e723fc2e4d0f4d2796fe45eb979
pornpol <pornpol.w@gmail.com>
2019-12-06 14:31:22 +0700

    edit scroll height

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Scroll = ({ children }) => {
     return (
-        <div style={{ overflowY: 'scroll', border: '1px solid black', height: '500px' }}>
+        <div style={{ overflowY: 'scroll', border: '1px solid black', height: '800px' }}>
             {children}
         </div>
     )
