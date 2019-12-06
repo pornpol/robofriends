9435fea58eb1c464eee393b931a39f97ef9bf063
pornpol <pornpol.w@gmail.com>
2019-12-06 13:58:20 +0700

    Updates

---
+++ precache-manifest.2ce6a7b5de5a70808cd3a091b60fdebf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fd4b6e493c0e39ed7b21f696523e80",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d41a4f40930e37189b13",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1fe97c15877146fa9698",
+    "url": "/robofriends/static/css/main.fa5ca926.chunk.css"
+  },
+  {
+    "revision": "d41a4f40930e37189b13",
+    "url": "/robofriends/static/js/2.85c4fb15.chunk.js"
+  },
+  {
+    "revision": "1fe97c15877146fa9698",
+    "url": "/robofriends/static/js/main.802763fd.chunk.js"
+  },
+  {
+    "revision": "5645dfe01f57d087ff9e",
+    "url": "/robofriends/static/js/runtime-main.47a7d18e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
